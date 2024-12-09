a7b525e7656054ba6ec4b02014ed2bf067e6168f
teiyou <teiyou416@gmail.com>
2024-12-09 16:08:59 +0900

    getu1

--- 9.py
+++ 9.py
@@ -1,7 +1,20 @@
-n1 = int(input('give n1:'))
-n2 = int(input('give n2:'))
-def SumandPro(n1,n2):
-    sum = n1 + n2  
-    pro = n1 *n2 
-    print(f'the sum:{sum},the produce:{pro}')
-SumandPro(n1,n2)
+def gcd():
+    a = int(int(input("Enter the first number: ")))
+    b = int(input("Enter the second number: "))
+    while b != 0:
+        t = a % b
+        a = b
+        b = t
+    return a
+def sort(arr):
+    for i in range(len(arr)-1):
+        minindex = i
+        for j in range(i+1,len(arr)):
+            if arr[j]<arr[minindex]:
+                minindex = j
+        if i != minindex :
+            arr[i],arr[minindex] = arr[minindex],arr[i]
+        print(f"step {i+1} : {arr}")
+        print(f"minindex:{arr[minindex]}")
+arr = [5,2,3,1,8,9,0,7,4,6]
+
